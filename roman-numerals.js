893c3beae8d8a704d2664025cc540b9f6d369bb5
Robyn Heslop <robyn.g.heslop@gmail.com>
2020-12-02 14:50:22 +0800

    refactor

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,74 +1,58 @@
-const totalIValues = number => {
+const loopOverCharacter = (number, value) => {
     let returnString = '';
     for (let i = 0; i < number; i++) {
-        returnString += "I"
+        returnString += value
     }
     return returnString;
 }
 
-const totalXValues = number => {
-    let returnString = '';
-    for (let i = 0; i < number; i++) {
-        returnString += "X"
+const calculateDigit = (number, digit) => {
+    switch (digit) {
+        case 1:
+            if (number === 4) return "IV";
+            if (number === 9) return "IX";
+            let digit1String = "";
+            if (number >= 5) {
+                digit1String += "V";
+                digit1String += loopOverCharacter(number % 5, "I");
+                return digit1String;
+            }
+            return loopOverCharacter(number, "I");
+        case 10:
+            if (number === 4) return "XL";
+            if (number === 9) return "XC";
+            let digit10String = "";
+            if (number >= 5) {
+                digit10String += "L";
+                digit10String += loopOverCharacter(number % 5, "X");
+                return digit10String;
+            }
+            return loopOverCharacter(number, "X");
+        case 100:
+            if (number === 4) return "CD";
+            if (number === 9) return "CM";
+            let digit100String = "";
+            if (number >= 5) {
+                digit100String  += "D";
+                digit100String  += loopOverCharacter(number % 5, "C");
+                return digit100String ;
+            }
+            return loopOverCharacter(number, "C");
     }
-    return returnString;
-}
 
-const totalCValues = number => {
-    let returnString = '';
-    for (let i = 0; i < number; i++) {
-        returnString += "C"
-    }
-    return returnString;
-}
-
-const calculateLastDigit = number => {
-    if (number === 4) return "IV";
-    if (number === 9) return "IX";
-    let returnString = "";
-    if (number >= 5) {
-        returnString += "V";
-        returnString += totalIValues(number % 5);
-        return returnString;
-    }
-    return totalIValues(number);
-}
-
-const calculateSecondLastDigit = number => {
-    if (number === 4) return "XL";
-    if (number === 9) return "XC";
-    let returnString = "";
-    if (number >= 5) {
-        returnString += "L";
-        returnString += totalXValues(number % 5);
-        return returnString;
-    }
-    return totalXValues(number);
-}
-
-const calculateThirdLastDigit = number => {
-    if (number === 4) return "CD";
-    if (number === 9) return "CM";
-    let returnString = "";
-    if (number >= 5) {
-        returnString += "D";
-        returnString += totalCValues(number % 5);
-        return returnString;
-    }
-    return totalCValues(number);
 }
 
 const generateRomanNumerals = (number) => {
     let romanNumerals = "";
     if (number / 100 >= 1) {
-        romanNumerals += calculateThirdLastDigit(Math.floor(number / 100));
-        romanNumerals += calculateSecondLastDigit(Math.floor((number % 100) / 10));
-        romanNumerals += calculateLastDigit((number % 100) % 10)
+        romanNumerals += calculateDigit(Math.floor(number / 100), 100);
+        romanNumerals += calculateDigit(Math.floor((number % 100) / 10), 10);
+        romanNumerals += calculateDigit(((number % 100) % 10), 1)
     } else if (number / 10 >= 1) {
-        romanNumerals += calculateSecondLastDigit(Math.floor(number / 10))
-        romanNumerals += calculateLastDigit(number % 10)
+        romanNumerals += calculateDigit((Math.floor(number / 10)), 10)
+        romanNumerals += calculateDigit((number % 10), 1)
     } else {
-        romanNumerals += calculateLastDigit(number);
+        romanNumerals += calculateDigit(number, 1);
     }
     return romanNumerals;
 }
